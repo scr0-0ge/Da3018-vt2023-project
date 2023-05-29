67ade1b52d22d4e25820ef8663355007c1ac2e8f
scr0-0ge <82386387+scr0-0ge@users.noreply.github.com>
2023-05-29 16:38:45 +0200

    Day 2 Update GenomeAssemblyGraph.java
    Refine overlap calculations and calculate the number of connected components with at least three vertices in the genome assembly graph.

--- GenomeAssemblyGraph.java
+++ GenomeAssemblyGraph.java
@@ -3,11 +3,13 @@ import java.util.*;
 
 class Contig {
     int length;
-    Set<Integer> overlaps;
+    Set<String> overlaps;
+    boolean visited;
 
     public Contig(int length) {
         this.length = length;
         overlaps = new HashSet<>();
+        visited = false;
     }
 }
 
@@ -23,22 +25,62 @@ public class GenomeAssemblyGraph {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] parts = line.split("\\s+");
-                String id1 = parts[0];
-                String id2 = parts[1];
-                int length1 = Integer.parseInt(parts[7]);
-                int length2 = Integer.parseInt(parts[11]);
+                String id1 = parts[0]; // ID of the first contig
+                String id2 = parts[1]; // ID of the second contig
+                int length1 = Integer.parseInt(parts[7]); // length of the first contig
+                int length2 = Integer.parseInt(parts[11]); // length of the second contig
+    
+                // Calculate the overlap length in both contigs
+                int overlapLength1 = Integer.parseInt(parts[6]) - Integer.parseInt(parts[5]);
+                int overlapLength2 = Integer.parseInt(parts[10]) - Integer.parseInt(parts[9]);
+    
+                // Store contigs in the map if they are not already present
                 if (!contigMap.containsKey(id1)) {
                     contigMap.put(id1, new Contig(length1));
                 }
                 if (!contigMap.containsKey(id2)) {
                     contigMap.put(id2, new Contig(length2));
                 }
-                contigMap.get(id1).overlaps.add(id2.hashCode());
-                contigMap.get(id2).overlaps.add(id1.hashCode());
+    
+                // If overlap length is at least 1000 in both sequences, add overlap
+                if (overlapLength1 >= 1000 && overlapLength2 >= 1000) {
+                    contigMap.get(id1).overlaps.add(id2);
+                    contigMap.get(id2).overlaps.add(id1);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+
+    public int getComponentsWithAtLeastThreeVertices() {
+        int count = 0;
+
+        for (String id : contigMap.keySet()) {
+            if (!contigMap.get(id).visited) {
+                int componentSize = dfs(id);
+                if (componentSize >= 3) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private int dfs(String id) {
+        Contig contig = contigMap.get(id);
+        contig.visited = true;
+        int size = 1;
+
+        for (String neighborId : contig.overlaps) {
+            if (!contigMap.get(neighborId).visited) {
+                size += dfs(neighborId);
+            }
+        }
+
+        return size;
     }
 
     public void printDegreeDistribution(String outputPath) {
@@ -47,7 +89,7 @@ public class GenomeAssemblyGraph {
             int degree = contig.overlaps.size();
             degreeDist.put(degree, degreeDist.getOrDefault(degree, 0) + 1);
         }
-    
+
         // Writing to csv file
         try (PrintWriter writer = new PrintWriter(new File(outputPath))) {
             StringBuilder sb = new StringBuilder();
@@ -55,18 +97,18 @@ public class GenomeAssemblyGraph {
             sb.append(',');
             sb.append("Count");
             sb.append('\n');
-    
+
             for (Map.Entry<Integer, Integer> entry : degreeDist.entrySet()) {
                 sb.append(entry.getKey());
                 sb.append(',');
                 sb.append(entry.getValue());
                 sb.append('\n');
             }
-    
+
             writer.write(sb.toString());
-    
+
             System.out.println("Degree distribution data written to file: " + outputPath);
-    
+
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
@@ -77,6 +119,7 @@ public class GenomeAssemblyGraph {
         GenomeAssemblyGraph g = new GenomeAssemblyGraph();
         g.readData("G:\\project\\Da3018-vt2023-project\\Spruce_fingerprint_2017-03-10_16.48.olp.m4");
         g.printDegreeDistribution("G:\\project\\Da3018-vt2023-project\\github\\DegreeDistribution.csv");
-        // Further processing...
+        int componentCount = g.getComponentsWithAtLeastThreeVertices();
+        System.out.println("The number of components with at least three vertices: " + componentCount);
     }
 }
